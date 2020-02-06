cfe1faec04ec6667d390d5243ae059818634fdd9
vagrant <vagrant@localhost.localdomain>
2020-02-06 13:01:11 +0000

    workflow

--- handler.py
+++ handler.py
@@ -1,9 +1,16 @@
 import requests
+import unittest
 
 def main(event, context):
     response = requests.get('https://www.kabegiwablog.com/')
 
-    print(response)
+    return response.status_code
+
+
+class test(unittest.TestCase):
+    def test_main(self):
+        self.assertEqual(main('',''),200)
 
 if __name__ == '__main__':
-  main('', '')
+    unittest.main()
+    main('', '')
